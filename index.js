354a6ed50c673503be06b20d477e428a72e2c42f
doppleware <roni.dover@gmail.com>
2019-12-01 21:02:24 -0800

    v1 release

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const wait = require('./wait');
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    const token = core.getInput('colony_token');
+    const account = core.getInput('colony_account');
+    const space = core.getInput('colony_space');
+    var request = require('request');
+    var url = 'http://' + account + '.cloudshellcolony.com/api/spaces/'+space;
+    request.get(url + '/blueprints')
+    .auth(null, null, true, token)
+    .on('response', function(response) {
+      console.log(response.statusCode) // 200
+      console.log(response.headers['content-type']) // 'image/png'
+    })
+    core.setOutput('result', response.statusCode);
   } 
   catch (error) {
     core.setFailed(error.message);
